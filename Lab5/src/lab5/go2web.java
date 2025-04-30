c5a667bd8404f6094fd94f6a20722e06bde9821b
katymun <munkate2003@gmail.com>
2025-04-30 11:52:34 +0300

    feat: added classes helpers, some method in the main class

--- Lab5/src/lab5/go2web.java
+++ Lab5/src/lab5/go2web.java
@@ -1,10 +1,12 @@
 package lab5;
 
+import javax.net.ssl.SSLSession;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class go2web {
     private static final int DEFAULT_HTTP_PORT = 80;
@@ -26,9 +28,37 @@ public class go2web {
                         printHelp();
                         return;
                     }
+                    fetchUrl(args[1]);
+                    break;
+                case "-s":
+                    if (args.length < 2) {
+                        System.out.println("Error: Search term is required with -s option");
+                        printHelp();
+                        return;
+                    }
 
+                    StringBuilder searchTerm = new StringBuilder(args[1]);
+                    for (int i = 2; i < args.length; i++) {
+                        searchTerm.append(" ").append(args[i]);
+                    }
+                    searchTerm(searchTerm.toString());
+                    break;
+                case "-h":
+                    printHelp();
+                    break;
+                default:
+                    System.out.println("Unknown option: " + args[0]);
+                    printHelp();
+                    break;
             }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
         }
+    }
+
+    private static void searchTerm(String string) {
+
     }
 
     private static void printHelp() {
@@ -47,7 +77,7 @@ public class go2web {
         }
         URL parsedUrl = parseUrl(url);
         String host = parsedUrl.getHost();
-        String path = parsedUrl.getPath().isEmpty() ? "/";
+        String path = parsedUrl.getPath().isEmpty() ? "/" :  parsedUrl.getPath();
         String query = parsedUrl.getQuery();
         if (query != null) {
             path += "?" + query;
@@ -63,6 +93,60 @@ public class go2web {
         HttpResponse httpResponse = parseHttpResponse(response);
     }
 
+    private static HttpResponse parseHttpResponse(String response) {
+        HttpResponse httpResponse = new HttpResponse();
+
+        int headerBodySeparator = response.indexOf("\r\n\r\n");
+        if (headerBodySeparator == -1) {
+            httpResponse.body = "";
+            String headersStr = response;
+        } else {
+            String headersStr = response.substring(0, headerBodySeparator);
+            httpResponse.body = response.substring(headerBodySeparator + 4);
+
+            if (httpResponse.body.contains("\r\n")) {
+                String[] lines = httpResponse.body.split("\r\n");
+                if (lines.length > 1 && lines[0].matches("[0-9a-fA-F]+")) {
+                    StringBuilder decodedBody = new StringBuilder();
+                    int i = 0;
+                    while (i < lines.length) {
+                        if (lines[i].matches("[0-9a-fA-F]+")) {
+                            int chunkSize = Integer.parseInt(lines[i], 16);
+                            if (chunkSize == 0) break;
+                            i++;
+                            if (i < lines.length) {
+                                decodedBody.append(lines[i]);
+                                i++;
+                            }
+                        } else {
+                            i++;
+                        }
+                    }
+                    httpResponse.body = decodedBody.toString();
+                }
+            }
+
+            String[] headers = headersStr.split("\r\n");
+            if (headers.length > 0) {
+                String statusLine = headers[0];
+                String[] statusParts = statusLine.split(" ", 3);
+                if (statusParts.length >= 2) {
+                    httpResponse.statusCode = Integer.parseInt(statusParts[1]);
+                }
+
+                for (int i = 1; i < headers.length; i++) {
+                    int colonPos = headers[i].indexOf(":");
+                    if (colonPos > 0) {
+                        String nume = headers[i].substring(0, colonPos).trim();
+                        String value = headers[i].substring(colonPos + 1).trim();
+                        httpResponse.headers.put(nume, value);
+                    }
+                }
+            }
+        }
+        return httpResponse;
+    }
+
     private static String fetchHttpUrl(String host, String path) {
     }
 
@@ -74,5 +158,16 @@ public class go2web {
             url = "http://" + url;
         }
         return new URL(url);
+    }
+
+    private static String stripHtmlTags(String html) {
+        return html.replaceAll("<[^>]*>", "")
+                .replaceAll("&nbsp;", " ")
+                .replaceAll("&lt;", "<")
+                .replaceAll("&gt;", ">")
+                .replaceAll("&amp;", "&")
+                .replaceAll("&quot;", "\"")
+                .replaceAll("\\s+", " ")
+                .trim();
     }
 }
